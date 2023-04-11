59643c8e93ec4845bdc2ce790d184aff75100440
windowsboy111 <wboy111@outlook.com>
2023-04-11 13:49:56 +0800

    fix: builds.id no pri key

---
+++ migrations/20230411054105_build-no-prikey.sql
@@ -0,0 +1,16 @@
+-- This file is part of Madoguchi.
+--
+-- Madoguchi is free software: you can redistribute it and/or modify it under the terms of
+-- the GNU General Public License as published by the Free Software Foundation, either
+-- version 3 of the License, or (at your option) any later version.
+--
+-- Madoguchi is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+-- without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+-- See the GNU General Public License for more details.
+--
+-- You should have received a copy of the GNU General Public License along with Madoguchi.
+-- If not, see <https://www.gnu.org/licenses/>.
+--
+DELETE FROM builds *;
+ALTER TABLE builds DROP id;
+ALTER TABLE builds ADD id VARCHAR(255) NOT NULL;
