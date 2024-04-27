20a7bab82037c539f1fad9d25d43fd1dc50c5283
madomado <madonuko@outlook.com>
2024-04-27 17:28:57 +0800

    chore: strip useless crates and tidy up

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,7 @@
 // You should have received a copy of the GNU General Public License along with Madoguchi.
 // If not, see <https://www.gnu.org/licenses/>.
 //
+#[allow(unused_imports)]
 mod api;
 mod db;
 use rocket::*;
