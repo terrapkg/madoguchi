452d2352d21038708c144c875a59ffd83b067627
madonuko <madonuko@outlook.com>
2025-06-17 17:01:38 +0800

    chore(api/auth): add license text

--- src/api/auth.rs
+++ src/api/auth.rs
@@ -1,3 +1,16 @@
+/// This file is part of Madoguchi.
+///
+/// Madoguchi is free software: you can redistribute it and/or modify it under the terms of
+/// the GNU General Public License as published by the Free Software Foundation, either
+/// version 3 of the License, or (at your option) any later version.
+///
+/// Madoguchi is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+/// without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+/// See the GNU General Public License for more details.
+///
+/// You should have received a copy of the GNU General Public License along with Madoguchi.
+/// If not, see <https://www.gnu.org/licenses/>.
+///
 use base64::{engine::general_purpose::STANDARD_NO_PAD, Engine};
 use jwt_simple::prelude::*;
 use rocket::{
